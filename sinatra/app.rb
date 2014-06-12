8f5892dfd1cd02b659f24223259e5ceeaeee7165
Nick Shook <nicholas.shook@gmail.com>
2014-06-11 20:11:44 -0700

    Updated Sinatra app to render views

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,9 +1,22 @@
 require 'sinatra'
+require 'data_mapper'
+DataMapper.setup(:default,
+                 ENV['DATABASE_URL'] ||
+                 "sqlite3://#{Dir.pwd}/development.db")
+
+class Contact
+  include DataMapper::Resource
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :email,        String, :required => true
+end
+DataMapper.finalize
 
 get '/' do
-  @contacts = Contact.all
+  @contacts = Contact.all || []
+  erb :index
 end
 
 post '/contacts' do
-  Contact.create!(name: params[:name], email: params[:email])
+  Contact.create(name: params[:name], email: params[:email])
 end
---
+++ sinatra/contact.rb
@@ -0,0 +1,5 @@
+require 'sqlite3'
+
+class Contact
+
+end
